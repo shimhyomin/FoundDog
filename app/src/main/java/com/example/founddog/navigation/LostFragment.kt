fa68dc11b7da915aed872e2f78bcff6cbacbe7cd
shimhyomin <shimhm03@gmail.com>
2021-07-30 20:57:54 +0900

    FIX LostFragment Item List UI

--- app/src/main/java/com/example/founddog/navigation/LostFragment.kt
+++ app/src/main/java/com/example/founddog/navigation/LostFragment.kt
@@ -2,6 +2,7 @@ package com.example.founddog.navigation
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,6 +10,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.core.content.res.ResourcesCompat
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -39,6 +42,12 @@ class LostFragment : Fragment() {
 
         recyclerview.adapter = RecyclerViewAdapter()
         recyclerview.layoutManager = LinearLayoutManager(activity)
+
+        try {
+            recyclerview.addItemDecoration(DividerItemDecoration(activity,LinearLayoutManager.VERTICAL))
+        }catch (e: Exception){
+            Log.d(TAG, "error : "+e)
+        }
 
         Log.d(TAG, "after recyclerview")
     }
@@ -95,8 +104,14 @@ class LostFragment : Fragment() {
             Log.d(TAG, "img_recyclerview : " + img_recycle)
             Log.d(TAG, "img : " + img)
 
-            Glide.with(holder.itemView.context).load(postDTO[position].imgUrl).into(img)
-            viewholder.text_recycle.text = postDTO[position].title
+            val drawable: Drawable? = ResourcesCompat.getDrawable(resources, R.drawable.placeholder, null)
+
+            Glide.with(holder.itemView.context)
+                .load(postDTO[position].imgUrl)
+                .thumbnail(0.1f)
+                .placeholder(drawable)
+                .into(img)
+            viewholder.title_recycle.text = postDTO[position].title
 
             holder.itemView.setOnClickListener {
                 val intent = Intent(context, DetailActivity::class.java)
